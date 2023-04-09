e2684a424627ea57aad05b18579d60d435579782
Bill Harding <wbharding@gmail.com>
2023-04-08 22:49:40 -0700

    Reformatting

--- plugin.js
+++ plugin.js
@@ -1,9 +1,23 @@
 const plugin = {
+  // --------------------------------------------------------------------------------------
   constants: {
     defaultSystemPrompt: "You are a helpful assistant.",
     pluginName: "Yappy",
+    tokenReplacePrompts: {
+      code: `Provide a snippet of source code (in either Javascript, Python or Ruby) implementing
+      the intent of the text preceding the token. Use the triple backtick to begin and end your code.`,
+      lolz: `Provide up to 100 words of entertaining, humorous content that is a little bit edgy. 
+      Reference other text in the document provided to show how well you understand it, especially the text 
+      near the replace token. If you can't figure out a funny joke, insert a four line limerick, song, 
+      poem, or rap that is relevant to the nearby text.`,
+      sum: `Calculate the sum of the table cells in the row or column of the token. 
+      Respond only with plain text to be placed in the table cell. 
+      If the summed cells contain a unit, include that unit in your response. The limit of your response is 20 characters.
+      If you can not find numbers to sum, respond with "🤔"`,
+    },
   },
 
+  // --------------------------------------------------------------------------------------
   insertText: {
     "lolz": async function(app) {
       return await this._contextAwarePrompt(app, "lolz");
@@ -22,6 +36,7 @@ const plugin = {
     },
   },
 
+  // --------------------------------------------------------------------------------------
   noteOption: {
     "revise": async function(app, noteUUID) {
       const instruction = await app.prompt("How should this note be revised?");
@@ -50,6 +65,7 @@ const plugin = {
     },
   },
 
+  // --------------------------------------------------------------------------------------
   replaceText: {
     "complete": async function(app, text) {
       const result = await this._callOpenAI(app, "replaceTextComplete", text);
@@ -67,6 +83,7 @@ const plugin = {
     },
   },
 
+  // --------------------------------------------------------------------------------------
   async _callOpenAI(app, promptType, promptContent) {
     let messages = [];
     const systemPrompt = this._systemPrompts[promptType] || this.constants.defaultSystemPrompt;
@@ -91,10 +108,7 @@ const plugin = {
           "Authorization": `Bearer ${ app.settings["API Key"] }`,
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({
-          model,
-          messages,
-        })
+        body: JSON.stringify({ model, messages, })
       });
       const result = await response.json();
       const { choices: [ { message: { content } } ] } = result;
@@ -105,25 +119,32 @@ const plugin = {
     }
   },
 
+  // --------------------------------------------------------------------------------------
   // GPT-3.5 has a 4097 token limit, so very much approximating that limit with this number
   _truncate(text, limit = 15000) {
     return text.length > limit ? text.slice(0, limit) : text;
   },
 
+  // --------------------------------------------------------------------------------------
   async _contextAwarePrompt(app, promptEm) {
     const noteUUID = app.context.noteUUID;
     const note = await app.notes.find(noteUUID);
     const noteContent = await note.content();
     const tokenLabel = `{${ this.constants.pluginName }: ${ promptEm }}`;
     const specificityWords = [ "code", "complete", "lolz" ].includes(promptEm) ? "text" : "only the exact word or words";
-    const tokenReplacePrompt = `Respond with ${ specificityWords } that could be used to replace the token <token> in the following input markdown document, which begins and ends with triple tildes:`;
-    const prompt = this._tokenReplacePrompts[promptEm](tokenLabel);
-    const appendMessage = "The resulting text should be grammatically correct and make sense in context. Do not explain how you derived your answer. Do not explain why you chose your answer. Do not respond with the token itself."
+    const tokenReplacePrompt = `Respond with ${ specificityWords } that could be used to replace the token <token> 
+      in the following input markdown document, which begins and ends with triple tildes:`;
+    const prompt = this.constants.tokenReplacePrompts[promptEm];
+    const appendMessage = `The resulting text should be grammatically correct and make sense in context. 
+      Do not explain how you derived your answer. Do not explain why you chose your answer. 
+      Do not respond with the token itself.`;
     if (noteContent.includes(tokenLabel)) {
-      const messages = [ tokenReplacePrompt,
+      const messages = [
+        tokenReplacePrompt,
         prompt.length ? prompt : null,
         `~~~${ noteContent.replace(tokenLabel, "<token>") }~~~`,
-        appendMessage ].filter(n => n);
+        appendMessage
+      ].filter(n => n);
       console.log("Composed messages for sending", messages);
       const result = await this._callOpenAI(app, promptEm, messages);
       return result;
@@ -133,6 +154,7 @@ const plugin = {
     }
   },
 
+  // --------------------------------------------------------------------------------------
   _systemPrompts: {
     replaceTextComplete: "You are a helpful assistant helping continue writing markdown-formatted content.",
     reviseContent: "You are a helpful assistant that revises markdown-formatted content, as instructed.",
@@ -140,20 +162,7 @@ const plugin = {
     summarize: "You are a helpful assistant that summarizes notes that are markdown-formatted.",
   },
 
-  _tokenReplacePrompts: {
-    code: () => `Provide a snippet of source code (in either Javascript, Python or Ruby) implementing
-      the intent of the text preceding the token. Use the triple backtick to begin and end your code.`,
-    complete: tokenLabel => ``,
-    lolz: () => `Provide up to 100 words of entertaining, humorous content that is a little bit edgy. 
-      Reference other text in the document provided to show how well you understand it, especially the text near the replace token. 
-      If you can't figure out a funny joke, insert a four line limerick, song, poem, or rap that is relevant to the nearby text.`,
-    lookup: () => "",
-    sum: tokenLabel => `Calculate the sum of the table cells in the row or column of the token. 
-      Respond only with plain text to be placed in the ${ tokenLabel } table cell. 
-      If the summed cells contain a unit, include that unit in your response. The limit of your response is 20 characters.
-      If you can not find numbers to sum, respond with "🤔"`,
-  },
-
+  // --------------------------------------------------------------------------------------
   _userPrompts: {
     replaceTextComplete: content => `Continue the following markdown-formatted content:\n\n${ content }`,
     reviseContent: ([ instruction, content ]) => [ instruction, content ],
